0e19db29bd553be6ef175a0fc45da52652f5563f
Daniel Atuma <atumadann@gmail.com>
2021-06-14 12:50:54 +0100

    Added routes

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,3 +1,11 @@
 let express = require('express')
 
 let userRouter = require('./user')
+let postRouter = require('./post')
+
+var app = express()
+
+app.use("/user/", userRouter)
+app.use("/post/", postRouter)
+
+module.exports = app
